fa3f77f97dbc0d83a9d801b7a22a76bab3fbefcf
Naveen Singh <naveen09cool@gmail.com>
2022-06-20 11:03:07 +0530

    Favs done

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ searchBtn.addEventListener("click", searchMealFunc);
 searchTextElement.addEventListener("input", searchMealFunc);
 backButton.addEventListener("click", closeRecipeDetails);
 resultCardsContainer.addEventListener("click", getRecipeDetails);
-// resultCardsContainer.addEventListener("click", addToFavourites);
+resultCardsContainer.addEventListener("click", addToFavourites);
 
 
 function searchMealFunc() {
@@ -33,10 +33,22 @@ function searchMealFunc() {
               <div class="card-body">
                   <h5 class="card-title">${meal.strMeal}</h5>
                   <a href="#" class="btn btn-primary recipe-button">Get Recipe</a>
-                  <button type="submit" class="btn btn-danger favourite-button"> Favourite </button>
               </div>
-          </div>
           `;
+          let favs = JSON.parse(localStorage.getItem("favourites"));
+          let addedCheck = false;
+          if(favs != null)
+            addedCheck = favs.includes(meal.idMeal);
+
+          if(addedCheck){
+            list += `
+              <button type="button" class="btn btn-sm btn-danger added-button"><a href="./favourite/favourite.html" style="text-decoration:none; color:inherit;"><i class="fa-solid fa-heart"></i> Added to Favourites</a></a></button>
+          </div>`;
+          }
+          else{
+            list += `<button type="submit" class="btn btn-danger favourite-button"> Add Favourite </button>
+          </div>`;
+          }
         });
       }
       // append all meals found to meal div
@@ -77,6 +89,42 @@ function addRecipeDetail(meal) {
 
 // To close recipe details
 function closeRecipeDetails(){
-  mainContainer.setAttribute('style','background-color:#e6e6e6')
+  mainContainer.setAttribute('style','background-color:#e6e6e6');
   mealDetailsContainer.parentElement.classList.remove("show-recipe");
+}
+
+// add meals to favourites
+function addToFavourites(event) {
+  // check if user has clicked on favourite button
+
+  // if user has not clicked on favourite button just return
+  if (!event.target.classList.contains("favourite-button")) {
+    return;
+  }
+
+  console.log(event.target);
+
+  let mealId = event.target.parentElement.id;
+  let favouriteMeals;
+
+  if (localStorage.getItem("favourites") === null) {
+    favouriteMeals = [];
+  } else {
+    favouriteMeals = JSON.parse(localStorage.getItem("favourites"));
+  }
+
+  // check if the mealId is already present
+  if (favouriteMeals.indexOf(mealId) !== -1) {
+    return;
+  }
+  // add id to array and save it back to local storage
+  favouriteMeals.push(mealId);
+  localStorage.setItem("favourites", JSON.stringify(favouriteMeals));
+
+  event.target.classList.remove("favourite-button");
+  event.target.classList.remove("btn-outline-primary");
+  event.target.classList.add("btn-danger");
+  event.target.classList.add("added-button");
+  event.target.innerHTML = `<a  href="./favourite/favourite.html" style="text-decoration:none; color:inherit;"><i class="fa-solid fa-heart"></i> Added to Favourites</a></a>`;
+
 }
